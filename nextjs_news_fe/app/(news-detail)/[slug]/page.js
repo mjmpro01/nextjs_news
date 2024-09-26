e90c1b6e6b4608cf925771ff8974a20b0dbd4bb1
phucdinhh <phuc.dinhngoc.7@gmail.com>
2024-09-26 08:56:22 +0700

    add news detail page

---
+++ nextjs_news_fe/app/(news-detail)/[slug]/page.js
@@ -0,0 +1,74 @@
+import { ChatBubbleBottomCenterIcon, ShareIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+
+import { images } from '@/assets/images'
+import NewsCard from '@/components/newsCard'
+
+const News = () => (
+  <>
+    <Image src={images.xperia} width={595} height={100} alt="" className="w-full" />
+
+    <div className='flex items-start gap-[10px] py-[10px]'>
+      <article className='basis-3/4'>
+        <div className='flex items-center gap-[10px] text-[10px] py-[10px]'>
+          <p>Diễn đàn</p>
+          <p>{">"}</p>
+          <p>Điện tử - Tiêu dùng</p>
+        </div>
+        <div>
+
+        </div>
+        <div className='flex justify-end gap-[10px] py-[10px] w-full'>
+          <div className='text-[14px] flex items-center gap-[5px]'>
+            <ChatBubbleBottomCenterIcon width={24} />
+            <p>Bình luận</p>
+          </div>
+
+          <div className='text-[14px] flex items-center gap-[5px]'>
+            <ShareIcon width={24} />
+            <p>Chia sẻ</p>
+          </div>
+        </div>
+      </article>
+
+      <div className=' basis-1/4 w-full flex flex-col gap-[20px]'>
+        <p className="text-[19px] text-[#000] font-semibold px-[10px]">
+          Bài mới
+        </p>
+        {Array.from({ length: 5 }).map(index => (
+          <NewsCard
+            key={index}
+            isHorizontal
+            bigThumbHorizontal
+            hasExcerpt={false}
+          />
+        ))}
+
+        <p className="text-[19px] text-[#000] font-semibold px-[10px]">
+          Bài nổi bật
+        </p>
+        {Array.from({ length: 5 }).map(index => (
+          <NewsCard
+            key={index}
+            isHorizontal
+            bigThumbHorizontal
+            hasExcerpt={false}
+          />
+        ))}
+
+        {Array.from({ length: 3 }).map(index => (
+          <Image
+            key={index}
+            src={images.icloudThumb}
+            width={300}
+            height={209}
+            alt=''
+            className=' aspect-square w-full h-auto'
+          />
+        ))}
+      </div>
+    </div>
+  </>
+)
+
+export default News
